a1bec88a02b2d180f09160d0d304ad216da65c67
Pratham Sanan <sananpratham.06@gmail.com>
2025-12-13 22:35:18 +0530

    chore: add passlib dependency for password hashing

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -1,7 +1,28 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Depends
+from sqlalchemy.orm import Session
+
+from app.schemas.user import UserCreate
+from app.models.user import User
+from app.core.database import SessionLocal
+from app.core.security import hash_password
 
 router = APIRouter(prefix="/api/auth", tags=["Auth"])
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 @router.post("/register", status_code=status.HTTP_201_CREATED)
-def register():
-    return {"message": "User registered"}
+def register(user: UserCreate, db: Session = Depends(get_db)):
+    new_user = User(
+        email=user.email,
+        password=hash_password(user.password)
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return {"message": "User registered successfully"}
